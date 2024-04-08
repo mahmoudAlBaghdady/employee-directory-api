ec2f964174edb5041f231cf5d95db8a8c455d426
mahmoudAlBaghdady <mahmoudalbaghdadyy@gmail.com>
2024-04-09 00:32:47 +0300

    Update the CI workflow and tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 /** @type {import('jest').Config} */
 const config = {
+  testEnvironment: "node",
   verbose: true,
   testTimeout: 60000,
+  globalSetup: "./tests/common/globalSetup.js",
+  globalTeardown: "./tests/common/globalTeardown.js",
 };
 
 module.exports = config;
